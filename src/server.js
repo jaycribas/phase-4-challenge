4023e5058e1676caf7d89cc667eb10b74a99865e
Jay Cribas <feverjay@gmail.com>
2017-11-13 23:23:19 -0800

    Index, basic navbar, main image and sidebar

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ require('ejs')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-app.use(express.static('public'))
+app.use(express.static('src/public'))
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
