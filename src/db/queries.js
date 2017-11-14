7b03830c0bcb2a508441bb60fc1e5fbd4f263dfb
Jay Cribas <feverjay@gmail.com>
2017-11-14 12:14:43 -0800

    Builds home page, routes from db using pg-p

---
+++ src/db/queries.js
@@ -0,0 +1,38 @@
+const db = require('./db')
+
+const getAlbums = () => {
+  return db.many(`
+    SELECT * FROM albums
+  `)
+}
+
+const getAlbumsByID = (id) => {
+  return db.one(`
+    SELECT
+      *
+    FROM
+      albums
+    WHERE
+      id = $1::int
+  `, id)
+}
+
+const getRecentReviews = () => {
+  return db.many(`
+    SELECT
+      *, TO_CHAR(posted_on, 'MM/DD/YYYY') AS date
+    FROM
+      reviews
+    JOIN
+      albums ON reviews.album_id = albums.id
+    ORDER BY
+      posted_on DESC
+    LIMIT
+      3
+  `)
+}
+
+module.exports = {
+  getAlbums,
+  getRecentReviews
+}
