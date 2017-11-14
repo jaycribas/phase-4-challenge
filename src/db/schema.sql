dd5db58a80b471076ece76c6e78a8e2a246452d5
Jay Cribas <feverjay@gmail.com>
2017-11-14 11:21:01 -0800

    Expands schema to include users and reviews, builds out home page

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,5 +1,25 @@
+DROP TABLE IF EXISTS albums CASCADE;
 CREATE TABLE albums (
-  id SERIAL,
+  id SERIAL PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
   artist VARCHAR(255) NOT NULL
+);
+
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(80) NOT NULL,
+  email VARCHAR(120) NOT NULL UNIQUE,
+  password VARCHAR(120) NOT NULL,
+  joined_on DATE,
+  img_url TEXT
+);
+
+DROP TABLE IF EXISTS reviews;
+CREATE TABLE reviews (
+  id SERIAL,
+  album_id INT REFERENCES albums,
+  user_id INT REFERENCES users,
+  posted_on DATE,
+  body TEXT
 );
