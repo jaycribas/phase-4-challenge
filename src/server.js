1d57410c4a3a9f094d4d0187289653a95d61aa22
Jay Cribas <feverjay@gmail.com>
2017-11-15 10:08:15 -0800

    Installs airbnb linter

--- src/server.js
+++ src/server.js
@@ -1,27 +1,26 @@
-const path = require('path')
-const express = require('express')
-const bodyParser = require('body-parser')
-const routes = require('./server/routes')
-const morgan = require('morgan')
-const methodOverride = require('method-override')
-const middlewares = require('./server/middlewares')
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./server/routes');
+const morgan = require('morgan');
+const methodOverride = require('method-override');
+const middlewares = require('./server/middlewares');
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-const app = express()
+const app = express();
 
-require('ejs')
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static('src/public'))
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(middlewares.setDefaultResLocals)
-app.use(morgan('dev'))
-app.use(methodOverride('_method'))
+app.use(express.static('src/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(middlewares.setDefaultResLocals);
+app.use(morgan('dev'));
+app.use(methodOverride('_method'));
 
-app.use('/', routes)
+app.use('/', routes);
 
 app.listen(port, () => {
-  console.log(`Listening on http://localhost:${port}...`)
-})
+  console.log(`Listening on http://localhost:${port}...`);
+});
